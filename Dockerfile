5b9c622d5f5122dc5ff3f15fe45f6b2472bd3dd5
Keiran Raine <kr2@sanger.ac.uk>
2016-12-19 11:50:02 +0000

    Initial build code

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM  quay.io/wtsicgp/dockstore-cgpmap:1.0.4
+
+MAINTAINER  keiranmraine@gmail.com
+
+LABEL uk.ac.sanger.cgp="Cancer Genome Project, Wellcome Trust Sanger Institute" \
+      version="1.0.0" \
+      description="The CGP WXS pipeline for dockstore.org"
+
+USER  root
+
+ENV OPT /opt/wtsi-cgp
+ENV PATH $OPT/bin:$PATH
+ENV PERL5LIB $OPT/lib/perl5
+
+#ADD scripts/mapping.sh $OPT/bin/mapping.sh
+#ADD scripts/ds-wrapper.pl $OPT/bin/ds-wrapper.pl
+#RUN chmod a+x $OPT/bin/mapping.sh $OPT/bin/ds-wrapper.pl
+
+ADD build/apt-build.sh build/
+ADD build/perllib-build.sh build/
+ADD build/opt-build.sh build/
+
+RUN bash build/apt-build.sh
+RUN bash build/perllib-build.sh
+RUN bash build/opt-build.sh
+
+USER    ubuntu
+WORKDIR /home/ubuntu
+
+CMD ["/bin/bash"]
---
+++ build/opt-build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -uxe
+
+mkdir -p /tmp/downloads
+
+cd /tmp/downloads
+
+# cgpVcf
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpVcf/archive/v2.1.1.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
+# cgpPindel
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpPindel/archive/v2.0.10.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
+# cgpCaVEManPostProcessing
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpCaVEManPostProcessing/archive/1.6.6.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
+# cgpCaVEManWrapper
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpCaVEManWrapper/archive/1.10.0.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
+# VAGrENT
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/VAGrENT/archive/v3.2.0.zip
+mkdir /tmp/downloads/distro
+bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
+cd /tmp/downloads/distro
+./setup.sh $OPT
+cd /tmp/downloads
+rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
+
+rm -rf /tmp/downloads
