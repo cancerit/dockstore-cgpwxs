23c94d5faff13d5b628148b773da02feac85b480
Keiran Raine <kr2@sanger.ac.uk>
2016-12-19 15:43:33 +0000

    Some dependancies

--- build/perllib-build.sh
+++ build/perllib-build.sh
@@ -2,4 +2,17 @@
 
 set -eux
 
-echo "I currently do nothing, inplace to save build flow changes if needed later"
+apt-get -yq update
+# install all perl libs, identify by grep of build "grep 'Successfully installed' build.log"
+# much faster, items needing later versions will still upgrade
+# still install those that get an upgrade though as dependancies will be resolved
+
+apt-get -yq install libdist-checkconflicts-perl
+apt-get -yq install libeval-closure-perl
+apt-get -yq install libclass-singleton-perl
+apt-get -yq install libscalar-list-utils-perl
+apt-get -yq install libdatetime-locale-perl
+apt-get -yq install libdatetime-perl
+apt-get -yq install liblog-log4perl-perl
+apt-get -yq install libfile-type-perl
+apt-get -yq install libsort-key-perl
