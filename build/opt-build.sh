f09eeb339ebf1dd0aff5264bd57272a0d8704cac
Keiran Raine <kr2@sanger.ac.uk>
2017-01-06 12:41:00 +0000

    Update pindel version

--- build/opt-build.sh
+++ build/opt-build.sh
@@ -16,7 +16,7 @@ cd /tmp/downloads
 rm -rf distro.zip /tmp/downloads/distro /tmp/hts_cache
 
 # cgpPindel
-curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpPindel/archive/v2.0.10.zip
+curl -sSL -o distro.zip --retry 10 https://github.com/cancerit/cgpPindel/archive/v2.1.0.zip
 mkdir /tmp/downloads/distro
 bsdtar -C /tmp/downloads/distro --strip-components 1 -xf distro.zip
 cd /tmp/downloads/distro
